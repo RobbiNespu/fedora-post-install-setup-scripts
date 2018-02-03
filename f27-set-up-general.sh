05508f01eb9c384a36fa1528e5ad113ecd4c7fc8
David <david@elsewebdevelopment.com>
2018-02-03 12:31:54 +0000

    first commit

---
+++ f27-set-up-general.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+#=======================================================================================
+#
+#         FILE: XXXXX.sh
+#
+#        USAGE: XXXXX.sh
+#
+#  DESCRIPTION: Post-installation Bash script for Fedora 27 Workstation General
+#      WEBSITE:
+#
+#      OPTIONS: see function ’usage’ below
+# REQUIREMENTS: Fedora installed on your computer
+#         BUGS: ---
+#        NOTES: After installation you may perform these additional tasks:
+#             - Firefox "about:support" what is compositor? If 'basic' open "about:config"
+#               find "layers.acceleration.force-enabled" and switch to true, this will
+#               force OpenGL acceleration
+#             - Add scripts directory to path > add :$HOME/scripts to PATH in.bash_profile
+#             - Install HTTPS everywhere, privacy badger, ublock origin in Firefox/Chromium
+#             - Consider "sudo dnf install kernel-tools", "sudo cpupower frequency-set --governor performance"
+#             - Files > preferences > views > sort folders before files
+#             - Change shotwell import directory format to %Y/%m + rename lower case, import photos from external drive
+#             - UMS > un-tick general config > enable external network + check force network on interface is correct network (wlp2s0)
+#       AUTHOR: David Else
+#      COMPANY: Else Web Development
+#      VERSION: 1.0
+#      CREATED: 11-7-17
+#     REVISION: 28-12-17
+#=======================================================================================
+
+read -rp "What would you like this computer to be called (hostname)? " hostname
+hostnamectl set-hostname "$hostname"
+
+# Enable repositories
+sudo su -c 'dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm'
+# sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/26/winehq.repo and install winehq-stable
+
+# Update everything
+echo "Updating Fedora..."
+sudo dnf -y --refresh upgrade
+
+echo "Installing packages..."
+sudo dnf -y install libva-intel-driver gstreamer1-vaapi gstreamer1-libav ffmpeg mpv \
+	fuse-exfat gnome-tweak-tool gnome-shell-extension-auto-move-windows.noarch gnome-shell-extension-pomodoro \
+	java-1.8.0-openjdk keepassx transmission-gtk mkvtoolnix-gui borgbackup \
+	freetype-freeworld lshw mediainfo dolphin-emu mame virt-manager klavaro jack-audio-connection-kit
+
+# Add some alias
+cat >>"$HOME/.bashrc" <<EOL
+alias ls="ls -ltha --color --group-directories-first" # l=long listing format, t=sort by modification time (newest first), h=human readable sizes, a=print hidden files
+alias tree="tree -Catr --noreport --dirsfirst --filelimit 100" # -C=colorization on, a=print hidden files, t=sort by modification time, r=reversed sort by time (newest first)
+EOL
+
+# Change some desktop settings
+gsettings set org.gnome.settings-daemon.plugins.media-keys max-screencast-length 0 # Ctrl + Shift + Alt + R to start and stop screencast
+gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
+gsettings set org.gnome.desktop.interface clock-show-date true
+gsettings set org.gnome.desktop.session idle-delay 1200
+gsettings set org.gnome.desktop.input-sources xkb-options "['caps:backspace', 'terminate:ctrl_alt_bksp']"
+gsettings set org.gnome.shell.extensions.auto-move-windows application-list "['org.gnome.Nautilus.desktop:2', 'org.gnome.Terminal.desktop:3', 'code.desktop:1', 'firefox.desktop:1', 'wine.desktop:4']" # for the gnome-shell-extension-auto-move extension
+
+# Allow virtual machines that use fusefs to intall properly with SELinux
+sudo setsebool -P virt_use_fusefs 1
+
+# Increase the amount of inotify watchers for live-server and audio
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+
+# Set subpixel hinting for freetype-freeworld
+gsettings set org.gnome.settings-daemon.plugins.xsettings hinting slight
+gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing rgba
+echo "Xft.lcdfilter: lcddefault" | sudo tee ~/.Xresources
+
+# pacmd list-sinks | grep sample and see bitdepth available
+# pulseaudio --dump-resample-methods and see resampling available
+# Increase quality of Pulse audio sound MAKE SURE your interface can handle s32le 32bit rather than the default 16bit
+sudo sed -i "s/; default-sample-format = s16le/default-sample-format = s32le/g" /etc/pulse/daemon.conf
+sudo sed -i "s/; resample-method = speex-float-1/resample-method = speex-float-10/g" /etc/pulse/daemon.conf
+sudo sed -i "s/; avoid-resampling = false/avoid-resampling = true/g" /etc/pulse/daemon.conf # for pulse 11 only
+
+# Config Jack assuming jack has created the 95-jack.conf file we are going to overwrite
+sudo usermod -a -G jackuser "$USERNAME"
+
+# add our current user to the jackuser group
+printf "# Default limits for users of jack-audio-connection-kit\n\n@jackuser - rtprio 98\n@jackuser - memlock unlimited\n\n@pulse-rt - rtprio 20\n@pulse-rt - nice -20" | sudo tee /etc/security/limits.d/95-jack.conf # rewrite the config file
+
+# Create symbolic links for external hard drive folders
+ln -s /run/media/david/WD-Red-2TB/Media/Audio ~/Music
+ln -s /run/media/david/WD-Red-2TB/Media/Video ~/Videos
+ln -s /run/media/david/WD-Red-2TB/Media/Photos ~/Pictures
+
+echo
+echo "========================"
+echo " REBOOTING IN 1 MINUTE! "
+echo "========================"
+echo
+
+shutdown -r
---
+++ f27-set-up-web-development.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+#=======================================================================================
+#
+#         FILE: XXXXX.sh
+#
+#        USAGE: XXXXX.sh
+#
+#  DESCRIPTION: Post-installation Bash script for Fedora Workstation for web development
+#      WEBSITE:
+#
+#      OPTIONS: see function ’usage’ below
+# REQUIREMENTS: Fedora installed on your computer
+#         BUGS: ---
+#        NOTES: After installation you may perform these additional tasks:
+#             - Disable drag & drop in Filezilla, run Filezilla once to create the config
+#               sudo sed -i "s/Drag and Drop disabled\">0</Drag and Drop disabled\">1</g" ~/.config/filezilla/filezilla.xml
+#             - Change phpMyAdmin setting to allow logging into root with no password:
+#               sudo gedit /etc/phpMyAdmin/config.inc.php
+#               ---------------
+#               $cfg['Servers'][$i]['AllowNoPassword'] = TRUE;
+#               ---------------
+#             - Allow mod-write in Apache, make the following edit
+#               sudo gedit /etc/httpd/conf/httpd.conf
+#               ---------------
+#               # AllowOverride controls what directives may be placed in .htaccess files.
+#               # It can be "All", "None", or any combination of the keywords:
+#               #   Options FileInfo AuthConfig Limit
+#               #
+#               AllowOverride All
+#               ---------------
+#             - For PHP and Fedora 27 these config file changes are vital!
+#               sudo gedit /etc/php-fpm.d/www.conf
+#               ---------------
+#               user = $USERNAME (was apache)
+#               listen.acl_users = apache,nginx,$USERNAME
+#               ---------------
+#       AUTHOR: David Else
+#      COMPANY: Else Web Development
+#      VERSION: 1.0
+#      CREATED: 11-7-17
+#     REVISION: 29-11-17
+#=======================================================================================
+
+# Enable Microsoft repository for VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+echo "Installing packages..."
+
+# ffmpeg-libs is needed for h264 video in firefox, see general install script
+sudo dnf -y install php phpmyadmin php-mysqlnd php-opcache mariadb-server sendmail \
+    code chromium filezilla nodejs zeal ShellCheck
+
+# Install VS Code extensions
+code --install-extension HookyQR.beautify
+code --install-extension ban.spellright
+code --install-extension christian-kohler.path-intellisense
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension deerawan.vscode-dash
+code --install-extension foxundermoon.shell-format
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension ritwickdey.LiveServer
+code --install-extension shinnn.stylelint
+code --install-extension timonwong.shellcheck
+
+# Install Node packages
+sudo npm install -g npm-check eslint
+
+# install shfmt shell parser onto the system for vs code plugin shell-format
+chmod +x shfmt_v2.1.0_linux_amd64
+sudo mv shfmt_v2.1.0_linux_amd64 /usr/local/bin/shfmt
+
+# install wordpress cli
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+
+# Make a directory for websites/apps and add the permissions
+mkdir ~/sites
+sudo ln -s ~/sites /var/www/html                                 # make symbolic link from the apache web directory to your sites folder
+chcon -R unconfined_u:object_r:httpd_sys_rw_content_t:s0 ~/sites # tell SELinux that these files/directories are allowed to be modified by Apache
+sudo setsebool -P httpd_can_network_connect 1                    # !! maybe not needed since Fedora 27 updates? !! tell SELinux Apache can connect to the outside network
+
+# Change the "User apache" string in the config file to "User (the username of the current user)". For a development machine, it's more convenient to run Apache as the current user to simplify permissions problems
+sudo sed -i "s/User apache/User $USERNAME/g" /etc/httpd/conf/httpd.conf
+
+# Change PHP settings to mirror the production server, and allow more functionality
+upload_max_filesize=128M # namesco default setting
+post_max_size=128M       # namesco default setting
+max_execution_time=60    # namesco default setting, Avada theme recommends 180
+
+for key in upload_max_filesize post_max_size max_execution_time; do
+    sudo sed -i "s/^\($key\).*/\1 = $(eval echo \${$key})/" /etc/php.ini
+done
+
+sudo systemctl start mariadb.service # start the MYSQL database
+sudo mysql_secure_installation       # finish installation of MYSQL to make it secure
