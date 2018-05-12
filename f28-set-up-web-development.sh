22838794b92819ecbcb9821bca95b2d97833f4a3
David <david@elsewebdevelopment.com>
2018-05-12 16:02:47 +0100

    edit exit command

--- f28-set-up-web-development.sh
+++ f28-set-up-web-development.sh
@@ -18,8 +18,7 @@
 #=======================================================================================
 
 if [ ! -f ./shfmt_v2.3.0_linux_amd64 ]; then
-    echo "shfmt_v2.3.0_linux_amd64 File not found!"
-    exit 1
+    echo "shfmt_v2.3.0_linux_amd64 File not found!" && exit 1
 fi
 
 # Enable Microsoft repository for VS Code
