0ca24c6877127c56e0bea11bc07b49fbfdae214d
David <david@elsewebdevelopment.com>
2018-05-13 12:33:35 +0100

    add npm init

--- f28-set-up-web-development-jsdom-quokka-AFTER-RUNNING-CODE-ONCE.sh
+++ f28-set-up-web-development-jsdom-quokka-AFTER-RUNNING-CODE-ONCE.sh
@@ -2,6 +2,7 @@
 
 # Install jsdom-quokka-plugin for quokka and set it to use the project's root directory index.html
 cd "$HOME/.quokka" || exit
+npm init -y
 npm install jsdom-quokka-plugin
 cat >"$HOME/.quokka/config.json" <<EOL
 {"plugins":["jsdom-quokka-plugin"],"jsdom":{"config":{"file":"index.html"}},"pro":false}
