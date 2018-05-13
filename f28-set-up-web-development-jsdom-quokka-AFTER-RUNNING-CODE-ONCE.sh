fb0b63b9890ee75f5cc0620a9cf07cbb2aeaf916
David <david@elsewebdevelopment.com>
2018-05-13 12:26:38 +0100

    initial commit

---
+++ f28-set-up-web-development-jsdom-quokka-AFTER-RUNNING-CODE-ONCE.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install jsdom-quokka-plugin for quokka and set it to use the project's root directory index.html
+cd "$HOME/.quokka" || exit
+npm install jsdom-quokka-plugin
+cat >"$HOME/.quokka/config.json" <<EOL
+{"plugins":["jsdom-quokka-plugin"],"jsdom":{"config":{"file":"index.html"}},"pro":false}
+EOL
